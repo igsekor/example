a685a83ba3c65fd02aa0d41090d31dd2482aeb73
Igor Korovchenko <igsekor@gmail.com>
2023-09-14 15:18:34 +0300

    Добавляет настройки

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+import math
+
+
+STRING_COUNT = 25
+LETTER_COUNT = 80
+TIMEOUT = 0.1
+LAND_POSITION = 23
+HELICOPTER = [
+    '-----≎-----   ⨂',
+    '  /¯¯¯¯¯\____/|',
+    ' <______/¯¯¯¯¯¯'
+]
+TRACK = [
+    { "x": 80, "y": 0 },
+    { "x": 40, "y": 0 },
+    { "x": 40, "y": 20 }
+]
